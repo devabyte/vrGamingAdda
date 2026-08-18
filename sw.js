f735cbbbee118f08859472bba218e9aa71784b91
devabyte <devabyte@users.noreply.github.com>
2026-08-18 19:34:45 +0000

    deploy: cee2a3c0ba6278163cb7477a74bed810162c5083

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='tableorder-v15';
-const ASSETS=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+const CACHE='tableorder-v16';
+const STATIC=['/icons/icon-192.png','/icons/icon-512.png','/manifest.json'];
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()));
 });
 
 self.addEventListener('activate',e=>{
@@ -10,10 +10,12 @@ self.addEventListener('activate',e=>{
 });
 
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    if(!res||res.status!==200||res.type!=='basic')return res;
-    const clone=res.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,clone));
-    return res;
-  })).catch(()=>caches.match('/index.html')));
+  const url=new URL(e.request.url);
+  // Always fetch index.html fresh from network
+  if(url.pathname.endsWith('.html')||url.pathname==='/'||url.pathname.endsWith('/')){
+    e.respondWith(fetch(e.request).catch(()=>caches.match('/index.html')));
+    return;
+  }
+  // Cache-first only for static assets (icons, manifest)
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
